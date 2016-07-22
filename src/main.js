43bc5f6e7eaec14da5c5719211b049166446d61b
Your Name <firehist@gmail.com>
2016-07-23 00:21:16 +0200

    Use ES6

--- src/main.js
+++ src/main.js
@@ -1,39 +1,35 @@
-var express = require('express');
-var config = require('./config.json');
-var User = require('./User.class.js');
-console.log(User)
+const express  = require('express')
 
-var app = express();
-var user = new User(config);
-var loginPromise;
+const User = require('./User.class.js')
+
+const app = express()
 
 app.set('json spaces', 2)
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  next();
-});
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  next()
+})
 
-app.listen(3000, function () {
-  console.log('listening on port 3000!');
-  loginPromise = user.login();
-});
+app.listen(3000, () => {
+  console.log('listening on port 3000!')
+})
 
-app.get('/getHeartbeat/:coords', function (request, response) {
-  var latitude = parseFloat(request.params.coords.split(',')[0], 10);
-  var longitude = parseFloat(request.params.coords.split(',')[1], 10);
+app.get('/getHeartbeat/:coords', (req, res) => {
+  var latitude = parseFloat(req.params.coords.split(',')[0], 10)
+  var longitude = parseFloat(req.params.coords.split(',')[1], 10)
   user
     .setLocation(latitude, longitude)
     .then(function(location) {
-      console.log(user.uuid, location);
-      return user.getHeartbeat();
+      console.log(user.uuid, location)
+      return user.getHeartbeat()
     })
-    .then(function(res) {
-      response.json(res);
+    .then(function(heartbeat) {
+      res.json(heartbeat)
     })
     .catch(function(err) {
-      response.json({'error': err});
-    });
-});
+      res.json({'error': err})
+    })
+})
