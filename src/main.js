a8363a63b71f46ca56ff7e9f091f1e195d7ee9da
Your Name <firehist@gmail.com>
2016-07-23 00:21:34 +0200

    Add route / to display available routes

--- src/main.js
+++ src/main.js
@@ -17,6 +17,14 @@ app.listen(3000, () => {
   console.log('listening on port 3000!')
 })
 
+app.get('/', (req, res) => {
+  let routes = app._router.stack
+    .filter(val => val.route)
+    .map(val => `${val.route.stack[0].method.toUpperCase()} ${val.route.path}`)
+
+  res.json(200, routes)
+})
+
 app.get('/getHeartbeat/:coords', (req, res) => {
   var latitude = parseFloat(req.params.coords.split(',')[0], 10)
   var longitude = parseFloat(req.params.coords.split(',')[1], 10)
